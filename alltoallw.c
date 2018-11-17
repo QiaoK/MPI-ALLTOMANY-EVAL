680158e106c63a3a555cea23e7c609f8c5722530
Wei-keng Liao <wkliao@users.noreply.github.com>
2018-11-16 22:44:06 -0600

    add alltoallw.c that implements the same all-to-many communication as isendrecv.c

--- alltoallw.c
+++ alltoallw.c
@@ -3,8 +3,7 @@
  * See COPYRIGHT notice in top-level directory.
  *
  * This program evaluates the performance of an all-to-many communication
- * pattern implemented using MPI asynchronous functions MPI_Isend, MPI_Irecv,
- * and MPI_Waitall.
+ * pattern, implemented using MPI_Alltoallw.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -45,10 +44,10 @@ int main(int argc, char** argv)
     extern int optind;
     int i, j, k, err, rank, nprocs, nprocs_node, nrecvs, span, blocklen;
     int verbose=0, ntimes, s_len, r_len, s_max, s_min;
+    int *s_size, *sdispls, *r_size, *rdispls;
     void *s_buf, *r_buf;
-    double t_post_irecv, t_post_isend, t_cur, t_wait, t_all, t_max;
-    MPI_Request *req;
-    MPI_Status *sts;
+    double t_all, t_max;
+    MPI_Datatype *dtypes;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -58,7 +57,6 @@ int main(int argc, char** argv)
     nprocs_node = 1;
     blocklen = 10;
     span = 8;
-    t_post_irecv = t_post_isend = t_wait = 0.0;
 
     /* command-line arguments */
     while ((i = getopt(argc, argv, "hp:b:n:")) != EOF)
@@ -97,71 +95,61 @@ int main(int argc, char** argv)
     /* number of receiver processes */
     nrecvs = nprocs / nprocs_node;
 
-    /* calculate message size per isend */
+    /* calculate message size per send */
     s_len = ((rank % nprocs_node) % span + 1) * blocklen;
     s_buf = malloc(s_len * nrecvs);
 
-    /* calculate message size for all irecv */
+    /* calculate message size for all recv */
     r_len = 0;
     if (rank % nprocs_node == 0)
         for (i=0; i<nprocs; i++)
             r_len += ((i % nprocs_node) % span + 1) * blocklen;
     r_buf = malloc(r_len);
 
-    req = (MPI_Request*) malloc(nprocs * 2 * sizeof(MPI_Request));
-    sts = (MPI_Status*) malloc(nprocs * 2 * sizeof(MPI_Status));
+    /* allocate and set the array of MPI datatypes */
+    dtypes = (MPI_Datatype*) malloc(nprocs * sizeof(MPI_Datatype));
+    for (i=0; i<nprocs; i++) dtypes[i] = MPI_BYTE;
+
+    /* allocate arrays of send/recv sizes and displacements */
+    s_size  = (int*) calloc(nprocs * 4, sizeof(int));
+    sdispls = s_size  + nprocs;
+    r_size  = sdispls + nprocs;
+    rdispls = r_size  + nprocs;
 
     MPI_Barrier(MPI_COMM_WORLD); /*-------------------------------------------*/
 
     t_all = MPI_Wtime();
     for (k=0; k<ntimes; k++) {
-        j = 0;
-
-        /* receivers (a subset of MPI_COMM_WORLD) */
-        t_cur = MPI_Wtime();
-        if (rank % nprocs_node == 0) {
-            char *ptr = (char*)r_buf;
-            for (i=0; i<nprocs; i++) {
-                r_len = ((i % nprocs_node) % span + 1) * blocklen;
-                if (verbose) printf("%2d recv from %2d: r_len=%d\n",rank,i,r_len);
-                err = MPI_Irecv(ptr, r_len, MPI_BYTE, i, 0, MPI_COMM_WORLD, &req[j++]);
-                err_handler(err, __LINE__, "MPI_Irecv");
-                ptr += r_len;
-            }
-        }
-        t_post_irecv += MPI_Wtime() - t_cur;
 
         /* senders (all processes in MPI_COMM_WORLD) */
-        t_cur = MPI_Wtime();
-        for (i=0; i<nrecvs; i++) {
-            char *ptr = (char*)s_buf;
+        s_size[0]  = s_len;
+        sdispls[0] = 0;
+        for (i=1; i<nrecvs; i++) {
             int r_rank = i * nprocs_node;
-            if (verbose) printf("%2d send to %2d: s_len=%d\n",rank,r_rank,s_len);
-            err = MPI_Isend(ptr, s_len, MPI_BYTE, r_rank, 0, MPI_COMM_WORLD, &req[j++]);
-            err_handler(err, __LINE__, "MPI_Isend");
-            ptr += s_len;
+            s_size[r_rank]  = s_len;
+            sdispls[r_rank] = s_len * i;
         }
-        t_post_isend += MPI_Wtime() - t_cur;
 
-        /* wait for all irecv/isend to complete */
-        t_cur = MPI_Wtime();
-        err = MPI_Waitall(j, req, sts);
-        err_handler(err, __LINE__, "MPI_Waitall");
-        t_wait += MPI_Wtime() - t_cur;
+        /* receivers (a subset of MPI_COMM_WORLD) */
+        if (rank % nprocs_node == 0) {
+            r_size[0]  = blocklen;
+            rdispls[0] = 0;
+            for (i=1; i<nprocs; i++) {
+                r_size[i]  = ((i % nprocs_node) % span + 1) * blocklen;
+                rdispls[i] = rdispls[i-1] + r_size[i-1];
+            }
+        }
+
+        err = MPI_Alltoallw(s_buf, s_size, sdispls, dtypes,
+                            r_buf, r_size, rdispls, dtypes, MPI_COMM_WORLD);
     }
     t_all = MPI_Wtime() - t_all;
 
     free(s_buf);
     free(r_buf);
-    free(req);
-    free(sts);
+    free(s_size);
+    free(dtypes);
 
-    MPI_Reduce(&t_post_irecv, &t_max, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-    t_post_irecv = t_max;
-    MPI_Reduce(&t_post_isend, &t_max, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-    t_post_isend = t_max;
-    MPI_Reduce(&t_wait, &t_max, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-    t_wait = t_max;
     MPI_Reduce(&t_all, &t_max, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
     t_all = t_max;
     MPI_Reduce(&s_len, &s_max, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
@@ -169,7 +157,7 @@ int main(int argc, char** argv)
 
     if (rank == 0) {
         printf("-----------------------------------------------------------\n");
-        printf("All-to-many communication implemented using MPI isend/irecv\n");
+        printf("All-to-many communication implemented using MPI_Alltoallw\n");
         printf("-----------------------------------------------------------\n");
         printf("Total number of MPI processes        = %d\n",nprocs);
         printf("Total number of sender   processes   = %d\n",nprocs);
@@ -178,9 +166,6 @@ int main(int argc, char** argv)
         printf("Message block unit size              = %d bytes\n",blocklen);
         printf("Max message size                     = %d bytes\n",s_max);
         printf("Min message size                     = %d bytes\n",s_min);
-        printf("Max time of posting irecv            = %.4f sec\n",t_post_irecv);
-        printf("Max time of posting isend            = %.4f sec\n",t_post_isend);
-        printf("Max time of MPI_Waitall()            = %.4f sec\n",t_wait);
         printf("Max end-to-end time                  = %.4f sec\n",t_all);
         printf("(Max means the maximum among all %d processes)\n",nprocs);
         printf("-----------------------------------------------------------\n");
